b20ebcd27fd3942a608dcd21c65f0d88838f8ad1
cygnus__karen <your_email@example.com>
2026-01-25 16:00:33 +0900

    update

--- project_dsl.rb
+++ project_dsl.rb
@@ -1,24 +1,24 @@
 # project.dsl.rb
-def build_project
-  Project.new("SampleProject") do
-    commonFiles "SampleIcons.png"
-    exportPath "./pak/"
-    #single addon making case
-    pakName "SampleAddon" do
-      pak "building.SampleAddon.pak"
-      dat "SampleAddon.dat"
-      png "SampleAddon.png", "Sample2.png"
-    end
+module Gungnir_DSL
+  def self.build_project
+    Project.new("SampleProject") do
+      commonFiles "SampleIcons.png"
+      exportPath "./pak/"
+      #single addon making case
+      pakName "SampleAddon" do
+        pak "building.SampleAddon.pak"
+        dat "SampleAddon.dat"
+        png "SampleAddon.png", "Sample2.png"
+      end
 
-    # multiple addon making case
-    pos = ["FB", "FR", "LB", "LR", "RB", "FL"]
+      # multiple addon making case
+      pos = ["FB", "FR", "LB", "LR", "RB", "FL"]
 
-    pakEach pos, prefix:"Angle-Fence" do | pos | # 派生系はprefix + posを付与して管理
-      pak "building.SampleAddon(#{pos}).pak"
-      dat "SampleAddon_#{pos}.dat"
-      png "SampleAddon#{pos}.png", "Sample2.png"
+      pakEach pos, prefix:"Angle-Fence" do | pos | # 派生系はprefix + posを付与して管理
+        pak "building.SampleAddon(#{pos}).pak"
+        dat "SampleAddon_#{pos}.dat"
+        png "SampleAddon#{pos}.png", "Sample2.png"
+      end
     end
   end
 end
-
-#end
