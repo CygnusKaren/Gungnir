e11b4fe9bf0b17a8344f63b453ad4da25ae96cee
cygnus__karen <your_email@example.com>
2026-01-25 13:26:12 +0900

    派生系アドオンの記述サンプルを追加

--- project_dsl.rb
+++ project_dsl.rb
@@ -2,10 +2,27 @@
 def build_project
   Project.new("SampleProject") do
     commonFiles "SampleIcons.png"
-    
+    exportPath "./pak/"
+    #single addon making case
     pakName "SampleAddon" do
       pak "building.SampleAddon.pak"
       dat "SampleAddon.dat"
       png "SampleAddon.png", "Sample2.png"
     end
+
+    # multiple addon making case
+    pos = ["FB", "FR", "LB", "LR", "RB", "FL"]
+
+    pakEach pos, prefix:"Angle-Fence" do | pos | # 派生系はprefix + posを付与して管理
+      pak "building.SampleAddon(#{pos}).pak"
+      dat "SampleAddon_#{pos}.dat"
+      png "SampleAddon#{pos}.png", "Sample2.png"
+    end
+  end
 end
+    # FENCE_POS := FB  FR LB LR RB FL
+    # FENCE_ANGLE_PAKS := \
+	# $(foreach v,$(FENCE_POS), \
+	#          $(DIR)building.YIC-railstop-platform-1_Diagonal\(Angle-Fence-$(v)\).pak)
+    
+#end
