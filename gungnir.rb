42e108a01fe45be5ed157909c6e7b5ee8ca66b9d
cygnus__karen <your_email@example.com>
2026-02-05 00:04:19 +0900

    デバッグモードの追加、このモードではビルドを行わない

--- gungnir.rb
+++ gungnir.rb
@@ -20,10 +20,10 @@ opts = OptionParser.new
 opts.on('-m MODE') { |v|
   params['m'] = v
 }
+opts.on('-d MODE') { |v|
+  params['d'] = v
+}
 opts.parse!(ARGV)
-
-p ARGV
-puts params
 
 # listen
 listener = Listen.to('./',only: /\.dat$|\.png|\.rb$/) do |modified, added, removed|
@@ -60,57 +60,61 @@ worker = Thread.new do
     next if files.empty?
     puts "処理開始"
     puts "まとめて処理: #{files}"
-    
-    case params['m'] # 引数"m"とオプション
-    when 'build'
-      #puts "ターゲット: #{files}"
-      # インデックスを走査する
-      files.each do | file |
-        b_file = File.basename(file)
 
-        # DSL定義かどうかを判定
-        if File.extname(file) == ".rb"
-          puts "DSLファイルが更新されました"
-          loadDSL
-          index = fileIndex()
-          export = exportPath()
-          next
-        end
-        
-        if index[b_file] # インデックスへの確認
-          # 該当あり
-          pp index[b_file] # 該当プロジェクトデータを出力
-          index[b_file].each do  |pak| # プロジェクトデータの切り出し
-            exportPath = "#{export}#{pak.pak_file}"
-            puts "Target:#{b_file} -> Project:#{pak.name}" # 更新ファイルとプロジェクトの紐付け
-            puts "ExportPath: #{exportPath}" # 出力ファイル名の出力
-            cmd = [
-              "makeobj_60-5",
-              "pak",
-              exportPath,
-              *pak.dat_files
-            ]
-            stdout , stderr, status = Open3.capture3(*cmd)
+    # DSLファイルの更新チェック 
+
+
+    # メイン処理
+    @cmd = []
+    files.each do | file |
+      # ファイル名を取得
+      b_file = File.basename(file)
+      if File.extname(file) == ".rb"
+        puts "DSLファイルが更新されました"
+        loadDSL
+        index = fileIndex()
+        export = exportPath()
+      end
+      # インデックスファイルを走査
+      if index[b_file] 
+        # 該当あり
+        pp index[b_file] # 該当プロジェクトデータを出力
+        index[b_file].each do  |pak| # プロジェクトデータの切り出し
+          exportPath = "#{export}#{pak.pak_file}"
+          puts "Target:#{b_file} -> Project:#{pak.name}" # 更新ファイルとプロジェクトの紐付け
+          puts "ExportPath: #{exportPath}" # 出力ファイル名の出力
+          # コマンドを生成する
+          @cmd = [
+            "makeobj_60-5",
+            "pak",
+            exportPath,
+            "#{pak.file_dirs}#{pak.dat_files.join(',')}"
+          ]
+
+          # Command execution
+          case params['m'] # 引数"m"とオプション
+          when 'build'
+            stdout, stderr, status = Open3.capture3(*@cmd)
             # 標準出力
             puts stdout
             # エラー
             puts stderr
             # status
             puts status
+          when 'debug' # デバッグモード
+            puts @cmd
+          else
+            # default
           end
-        else
-          # 該当無し
-            puts "Target:#{file} -> Project not found."
+          puts "========================"
         end
+      else
+        # 該当無し
+        puts "Target:#{file} -> Project not found."
       end
-      # system("makeobj ...")
-    when 'watch'
-    # watch 専用処理
-      puts #{files}
-    else
-      # default
     end
-    puts "========================"
+
+    
   end
 end
 
